b07cfd140deb79e696d091d14d06624291ac3c09
Ronak Adroja <ronakadroja1234@gmail.com>
2021-05-27 18:19:05 +0530

    First commit

---
+++ contactus.php
@@ -0,0 +1,17 @@
+<?php
+ if(isset($_POST['send'])){
+     $name = $_POST['Name'];
+     $email= $_POST['Sender'];
+     $subject= $_POST['Subject'];
+     $message= $_POST['Message'];
+     $from = "ronakadroja89@gmail.com";
+     $to = "ronakadroja1234@gmail.com";
+     $email_subject = "New form submitted from portfolio.";
+     $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject = " .$subject . "\r\n  Message =" . $message;
+     $headers = "From: " . $from . " \r\n Reply to: " . $email;
+     if($email!=NULL){
+         mail($to, $email_subject, $txt, $headers);
+         header("Location: thankyou.html");
+        }
+    }
+?>
