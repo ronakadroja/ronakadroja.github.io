54777b1f66e8fc79a9b9cde46ad10d14c17525ed
Ronak Adroja <84452183+ronakadroja@users.noreply.github.com>
2021-05-31 10:55:20 +0530

    y removal

--- contactus.php
+++ contactus.php
@@ -1,17 +1,36 @@
 <?php
- if(isset($_POST['send'])){
-     $name = $_POST['Name'];
-     $email= $_POST['Sender'];
-     $subject= $_POST['Subject'];
-     $message= $_POST['Message'];
-     $from = "ronakadroja89@gmail.com";
-     $to = "ronakadroja1234@gmail.com";
-     $email_subject = "New form submitted from portfolio.";
-     $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject = " .$subject . "\r\n  Message =" . $message;
-     $headers = "From: " . $from . " \r\n Reply to: " . $email;
-     if($email!=NULL){
-         mail("ronakadroja1234@gmail.com", $email_subject, $txt, $headers);
-         header("Location: thankyou.html");
-        }
-    }
+
+$EmailFrom = "ronakadroja89@gmail.com";
+$EmailTo = "ronakadroja1234@gmail.com";
+
+$subject = "New Message from Portfolio.";
+$Name = Trim(stripslashes($_POST['Name']));
+$Email = Trim(stripslashes($_POST['Sender']));
+$Subject = Trim(stripslashes($_POST['Subject']));
+$Message = Trim(stripslashes($_POST['Message']));
+
+$validationOK=true;
+if(!validationOK){
+    print "<meta http-equvia=\"refresh\" content=\"0,URL=error.htm\">";
+    exit;
+}
+
+$Body ="";
+$Body = "
+'
+Name : $Name
+Email : $Email
+Subject : $Subject
+Message : $Message
+'
+";
+
+$success = mail($EmailTo, $subject, $Body, "From : <$EmailFrom>", "Reply to : <$Email>");
+if($success){
+    print "<meta http-equvia=\"refresh\" content=\"0,URL=thankyou.html\">";
+}
+else{
+    print "<meta http-equvia=\"refresh\" content=\"0,URL=error.htm\">";
+}
+
 ?>
