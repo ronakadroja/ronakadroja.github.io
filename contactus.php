8589051130fcc05c7f3bcda040d700ea574d59b6
Ronak Adroja <84452183+ronakadroja@users.noreply.github.com>
2021-06-30 19:15:15 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $to = "ronakadroja1234@gmail.com";
     $mail_subject = "Message from Portfolio";
     $email_message = "Hello, Ronak,\n Name : $Name \n Email : $Email \n Subject : $Subject \n Message : $Message" ;
-    $from = "From: ronakadroja89@gmail.com";
+    $from = "From: $Email";
     
 
     if (mail($to, $mail_subject, $email_message, $from)) {
